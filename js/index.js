bcdb1a8122eb2e1371b341ab9ff3354a748b7dca
Raj Dave <10864@bpsdoha.edu.qa>
2022-08-26 09:01:24 +0300

    Major additions
    • Make boxes show the already present subject/section names
    • On click of rename button store those button values into the chrome storage
    • Many smaller changes

--- js/index.js
+++ js/index.js
@@ -13,25 +13,35 @@ Storage Structure
 // TODO : Make force dark mode for gcr
 
 const acc_number = 1;
-const subject_list = ["Main Class Group", "English", "History/Geography", "Economics/Civics", "Hindi", "Biology", "Physics", "Chemistry", "Math", "AI", "Mental Ability", "Art"];
-const section_list = ["Class Group"];
+let subject_list = [];
+let section_list = [];
+res = get_gcr_class_list().then( (result) => {
+    console.log(result);
+    for (let i = 0; i < result['subject_names'].length; i++) {
+        subject_list.push(result['subject_names'][i]);
+        section_list.push(result['section_names'][i]);
+        console.log(subject_list, section_list);
+    }
+});
+
 
 const submitBtn = document.getElementById('submit');
 const classroomURL = document.getElementById('gcr-url');
 const classesAmount = document.getElementById('classes-amount');
 const subjectsArea = document.getElementById('subjects-area');
 const sectionsArea = document.getElementById('sections-area');
-const BtnContainer = document.getElementById('lower');
+const renameBtn = document.getElementById('rename-btn');
+// const BtnContainer = document.getElementById('lower');
 const expForURL = new RegExp("https://classroom\\.google\\.com/u/\\d+");
 let gcr_input_hidden = false;
 let user_id;
-let renameButtonExists = false;
+// let renameButtonExists = false;
 
 
 toggle_url_input();
 function toggle_url_input() {
 // Hide URL input if user has entered it before
-    get_info().then((result) => {
+    get_info().then( (result) => {
         let gcr_url_input = document.getElementById("gcr-url-input");
         if (result == null) {
             gcr_url_input.style.display = "block";
@@ -45,13 +55,21 @@ function toggle_url_input() {
     })
 }
 
-submitBtn.addEventListener('click', () => {
+/*
+    >> Event Listeners
+
+       This part of the code is responsible for listening to events and performing actions accordingly.
+       It contains only event listeners.
+*/
+
+
+submitBtn.addEventListener('click', () => { // Event listener for the submit button
+
     let URL = classroomURL.value.trim();
     const amount = classesAmount.value.trim();
     let URLError;
     let amountError1;
     let amountError2;
-
 
     if(!expForURL.test(URL)) { // If the URL is not valid
         URLError = true;
@@ -72,7 +90,7 @@ submitBtn.addEventListener('click', () => {
         classesAmount.style.border = "1px solid white";
     }
 
-    if (parseInt(amount) < 0 || parseInt(amount) > 100) { // If amount between 1 and 100
+    if (parseInt(amount) < 1 || parseInt(amount) > 100) { // If amount between 1 and 100
         amountError2 = true;
         classesAmount.style.border = "1px solid red";
     } else {                // If amount is not a number
@@ -86,32 +104,50 @@ submitBtn.addEventListener('click', () => {
     if (!URLError && !amountError1 && !amountError2) {    // If both inputs are valid
         // Get user id from URL
         URL = URL.replace("https://classroom.google.com/u/", "").split("/");
+
         if (!isNaN(URL[0])) {
             user_id = URL[0];
         } else {
             user_id = URL[1];
         }
+
         console.info("User ID: " + user_id);
         set_info({"default_account": acc_number}).then( () => {
             console.info("Default account set to " + acc_number);
             subjectsArea.innerHTML = "";
             sectionsArea.innerHTML = "";
             create_boxes(amount, subjectsArea, sectionsArea);
-            create_rename_button();
+            toggle_rename_button("on");
             toggle_url_input();
         })
-
     }
 });
 
 
+renameBtn.addEventListener('click', () => { // Event listener for the rename button
 
-function init() { // This function should be run after the 4 variables are set
+    let subject_names = [];
+    let section_names = [];
+    let subject_inputs = document.getElementsByClassName("subject-box");
+    let section_inputs = document.getElementsByClassName("section-box");
+    for (let i = 0; i < subject_inputs.length; i++) {
+        subject_names.push(subject_inputs[i].value);
+    }
+    for (let i = 0; i < section_inputs.length; i++) {
+        section_names.push(section_inputs[i].value);
+    }
+    console.info("Subjects: " + JSON.stringify(subject_names));
+    console.info("Sections: " + JSON.stringify(section_names));
     set_gcr_class_list({
-        "subject_names": subject_list,
-        "section_names": section_list
+        "subject_names": subject_names,
+        "section_names": section_names
+    }).then( () => {
+        console.info("Class list set");
     });
-}
+
+});
+
+
 
 
 function set_info(account_info) {
@@ -149,11 +185,12 @@ function set_gcr_class_list(info) {
 }
 
 function get_gcr_class_list() {
-    return new Promise(function (resolve) {
-        chrome.storage.local.get(["class_list"], (result) => {
-            resolve(result);
-        })
-    })
+    return new Promise( (resolve) => {
+        chrome.storage.local.get(['class_list'], (result) => {
+            console.log(result.class_list);
+            resolve(JSON.parse(result.class_list));
+        });
+    });
 }
 
 
@@ -175,10 +212,12 @@ function create_boxes(number, subjectsArea, sectionsArea) {
 
         boxForSubject.setAttribute("type", "text");
         boxForSubject.setAttribute("class", "subject-box");
+        boxForSubject.setAttribute("value", subject_list[i]);
         boxForSubject.required = true;
-        
+
         boxForSection.setAttribute("type", "text");
         boxForSection.setAttribute("class", "section-box");
+        boxForSection.setAttribute("value", section_list[i]);
         boxForSection.style.marginLeft = "auto";
         boxForSection.required = true;
 
@@ -189,33 +228,15 @@ function create_boxes(number, subjectsArea, sectionsArea) {
         sectionsArea.appendChild(boxForSection);
     }
 
-
 }
 
-function create_rename_button() {
-    if (renameButtonExists) {
-        return;
+
+function toggle_rename_button(state) {
+    // hide and show rename button
+    if (state === "on") {
+        renameBtn.style.display = "block";
+    } else {
+        renameBtn.style.display = "none";
     }
-    const renameButton = document.createElement('button');
-
-    renameButton.setAttribute('type', 'button');
-    renameButton.id = "rename-btn";
-    renameButton.textContent = "Rename";
-
-    BtnContainer.appendChild(renameButton);
-    renameButtonExists = true;
-
-
 }
 
-/* Temporary code to mimic input gathering
-set_gcr_class_list({
-    "subject_names": subject_list,
-    "section_names": section_list
-}).then( () => {
-    set_info({"default_account": acc_number}).then( () => {
-        init();
-    })
-}) */
-
-init()
