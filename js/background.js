86b4fec11a60572a4e922d5be86efd4ad7a61d90
Raj Dave <10864@bpsdoha.edu.qa>
2023-06-18 18:08:15 +0300

    add backruond.js with one listener

---
+++ js/background.js
@@ -0,0 +1,11 @@
+console.log("Hello")
+
+
+chrome.storage.onChanged.addListener((changes, namespace) => {
+  for (let [key, { oldValue, newValue }] of Object.entries(changes)) {
+    console.log(
+      `Storage key "${key}" in namespace "${namespace}" changed.`,
+      `Old value was "${oldValue}", new value is "${newValue}".`
+    );
+  }
+});
