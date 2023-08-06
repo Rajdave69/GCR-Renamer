e6ea7bae80bcdcbba26449b642452d9daa2b1132
Raj Dave <10864@bpsdoha.edu.qa>
2023-08-06 11:56:32 +0300

    start working on config.js

---
+++ js/config.js
@@ -0,0 +1,25 @@
+// get the `?` parameters from URL
+const urlParams = new URLSearchParams(window.location.search);
+
+/*
+A valid URL should have the following parameters -
+
+courseIds=
+courseNames=
+sectionNames=
+teacherNames=
+pfpUrls=
+
+*/
+
+
+const courseIds = urlParams.get('courseIds').split(',');
+const courseNames = urlParams.get('courseNames').split(',');
+const sectionNames =  urlParams.get('sectionNames').split(',');
+const teacherNames = urlParams.get('teacherNames').split(',');
+const pfpUrls = urlParams.get('pfpUrls').split(',');
+const backgrounds = urlParams.get('backgrounds').split(',');
+
+
+console.log("JS loaded")
+
